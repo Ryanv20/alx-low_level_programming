fe814b5a9c399e03ebf373b6f1cee6240439fddd
Ryanv20 <kryan2743@gmail.com>
2022-09-15 15:36:00 -0700

    initial

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,10 +4,16 @@
  * main - prints positive or negative integer
  * Return: 0
  */
-int main (void)
+
+int main(void)
+
 {
 	int i;
-	i = 98;
+
+	i = 0;
+
 	positive_or_negative(i);
+
 	return (0);
+
 }
