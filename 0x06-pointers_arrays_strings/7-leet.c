9c20b7f6ef45c8f2359526d7c9ddfe70d6c2e00c
Ryanv20 <kryan2743@gmail.com>
2022-09-23 14:13:38 -0700

    ini

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,9 +13,9 @@ char *leet(char *s)
 	char *a = "aAeEoOtTlL";
 	char *b = "4433007711";
 
-	for (i = 0; s[i] != '\0'; o++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++;)
+		for (j = 0; j < 10; j++)
 		{
 			if (s[i] == a[j])
 			{
