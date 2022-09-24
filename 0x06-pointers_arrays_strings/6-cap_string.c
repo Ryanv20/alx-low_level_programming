b50d72ff42e8173350dbb35c891bb63da33434aa
Ryanv20 <kryan2743@gmail.com>
2022-09-23 17:17:29 -0700

    ini

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,50 +1,33 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: return length as integer;
+ * cap_string capitalize every word of a string
+ * @s: string to modify
+ *
+ * Return
  */
-
-int _strlen(char *s)
+char *cap_string(char *s)
 {
-	int len = 0;
+	int i, j;
 
-	while (*(s + len) != '\0')
-		len++;
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
-	return (len);
-}
-/**
- * cap_string - function that capitalize first character of a word
- * @str: string to capitalize
- * Return: returns the capitalize string
- */
-
-char *cap_string(char *str)
-{
-	int index = 0;
-
-	while (str[++index])
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (!(str[index] >= '0') && (str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')')
-			str[index] -= 32;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] < 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
 	}
 
-	return (str);
+	return (s);
 }
