546eca61c273f9308d718334efc612e8671f84f7
Ryanv20 <kryan2743@gmail.com>
2022-09-27 12:52:57 -0700

    task7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,9 @@
  */
 void print_diagums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	i = (row * size) + row;
+	diagonal_sum_2 += a[i];
+}
 
-	for (i = 0; i < (size * size); i++)
-	{
-		if (i % (size + 1) == 0)
-			sum1 += *(a + i);
-		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-			sum2 += *(a + i);
-	}
-	printf("%d, %d\n", sum1, sum2);
+printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2);
 }
