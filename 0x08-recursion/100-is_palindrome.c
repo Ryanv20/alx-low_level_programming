48d47d51a34f2abf76e67e1244aa62be7747641b
Ryanv20 <kryan2743@gmail.com>
2022-09-28 20:22:49 -0700

    task8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,28 +1,40 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * print_diagums - sums of the two diagonals of a square matrix of integers
- * @a: 2d arrays of chars
- * @size: number matrix
- * Return: void
+ * palindromeChecker - checks the string
+ * @str: the string to be checked
+ * @len: the length of the string
+ * @i: the incrementor, starts at 0
+ * Return: 1 if it's a palindrome, 0 if it's not
  */
-void print_diagsums(int *a, int size)
+int palindromeChecker(char *str, int len, int i)
 {
-	int i;
-	int sum1, sum2;
+	if (i < len && str[i] == str[len])
+		return (palindromeChecker(str, len - 1, i + 1));
+	if (str[i] != str[len])
+		return (0);
+	return (1);
+}
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to check the length of
+ * Return: an integer that displays the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 * _strlen_recursion(s + 1));
 
-	sum1 = 0;
-	sum2 = 0;
+	return (0);
+}
+/**
+ * is_palindrome - checks to see if a string is a palindrome
+ * @s: the string to check
+ * Return: 1 if it's a palindrome, 2 if it's not
+ */
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int length = _strlen_recursion(s) - 1;
 
-	i = 0;
-	while (i < size)
-	{
-		sum1 = sum1 + *(a + i * size + i);
-		sum2 = sum2 + *(a + i * size + size - i - 1);
-
-		i++;
-	}
-
-	printf("%i, %i\n", sum1, sum2);
+	return (palindromeChecker(s, length, i));
 }
