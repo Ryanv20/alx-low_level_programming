d8d08a3cda1aaa6efb37780266706e8af048d5f1
Ryanv20 <kryan2743@gmail.com>
2022-10-04 15:49:20 -0700

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
  * main - main funct
  * @argc: param count
  * @argv: param poiter
  * Return: int
  */
-
 int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argc[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
