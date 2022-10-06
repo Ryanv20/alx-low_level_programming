3c8b2f762fcf1b3c8eb442e43f71dfb7b02cff8e
Ryanv20 <kryan2743@gmail.com>
2022-10-05 21:58:47 -0700

    init

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -63,7 +63,9 @@ char **strtow(char *str)
 	{
 		for (i = 0; i <= len(str) && words; i++)
 		{
-			if ((str[i] != ' ') || (str[i] = '\0')) && i && ((str[i - 1] != ' '))
+			if ((str[i] != ' ') && (str[i] != '\0'))
+				size++;
+			else if ((str[i] != ' ') || (str[i] = '\0')) && i && ((str[i - 1] != ' '))
 			{
 				split[j] = (char *)malloc(sizeof(char) * size + 1);
 				if (split[j] != NULL)
