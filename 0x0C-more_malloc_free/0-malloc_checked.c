b9c49d3fc24f03aebb5f49827b4c205417483deb
Ryanv20 <kryan2743@gmail.com>
2022-10-07 21:22:57 -0700

    ini

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - A function that allocates memory using malloc.
+ * @b: integer nariable dat holds the memory size
+ * Author: Ryan
+ * Return: 0 means program was successful
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
