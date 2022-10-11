2de5fbd2e3c0171b3f8c63cde224945f54340ad4
Ryanv20 <kryan2743@gmail.com>
2022-10-11 08:57:09 -0700

    init

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the name
+ * main - Prints the name of the file program was complied from
  * Return: Always 0
  */
-
-int main(void)
 {
 printf("%s\n", _FILE_);
 return (0);
