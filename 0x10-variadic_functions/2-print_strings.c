ec958098ab477338be7b23e027f6dc52d6bbd73f
Ryanv20 <kryan2743@gmail.com>
2022-10-14 08:31:34 -0700

    init

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (i < n - 1)
 			if (separator)
-				printf("%s", seeparator);
+				printf("%s", separator);
 	}
 
 	printf("\n");
